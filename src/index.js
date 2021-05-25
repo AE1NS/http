91142b18a5d8a9c589013007cde850564ebdf8a3
Jan Gessinger <jgessinger@aeins.de>
2021-05-25 10:29:30 +0200

    cleanup

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const Http = registerPlugin('Http', {
-  web: () => import('./web').then(m => new m.HttpPluginWeb()),
-});
-export { Http };
-//# sourceMappingURL=index.js.map
